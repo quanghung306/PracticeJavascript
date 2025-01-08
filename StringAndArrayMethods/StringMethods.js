67ac2cce77608b5f93f6baaac78f7c3b0b62d1ee
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2025-01-08 11:38:54 +0700

    Added new StringMethods exercise

--- StringAndArrayMethods/StringMethods.js
+++ StringAndArrayMethods/StringMethods.js
@@ -35,3 +35,53 @@ function ReverseString(str) {
     console.log("🚀 ~ ReverseString ~ reversedStr:", reversedStr)
   }
   ReverseString(str);
+  // Kiểm tra chuỗi có bắt đầu hoặc kết thúc bằng một chuỗi con không
+  // Mô tả: Cho chuỗi "JavaScript is amazing!", kiểm tra xem chuỗi có bắt đầu bằng "Java" và kết thúc bằng "!" không.
+function CheckString() { 
+  const str= "JavaScript is amazing!"
+  const CheckString=str.startsWith('Java') && str.endsWith('!') ;
+  console.log(`Kiểm tra chuỗi có bắt đầu hoặc kết thúc bằng một chuỗi con không: ${CheckString}`)
+ }
+ CheckString();
+//  Kết hợp các chuỗi từ mảng
+//  Mô tả: Cho mảng chuỗi ["I", "am", "learning", "JavaScript"], kết hợp các phần tử trong mảng thành một chuỗi "I am learning JavaScript".
+function CombineStrings(){
+  const str =["I", "am", "learning", "JavaScript"];
+  const CombineStrings= str.join(' ')
+  console.log("🚀 ~ CombineStrings ~ CombineStrings:", CombineStrings)
+}
+CombineStrings();
+// Chuyển chuỗi thành mảng rồi lấy các phần tử đầu tiên và cuối cùng
+// Mô tả: Cho chuỗi "apple,banana,orange", chuyển chuỗi này thành mảng và lấy phần tử đầu tiên và cuối cùng.
+function getFirstAndLastElement() {
+  const str = "apple,banana,orange";
+  const FirstElement= str.split(",").shift()
+  const LastElement= str.split(",").pop()
+  console.log(`Phần tử đầu tiên là ${FirstElement} và phần tử cuối cùng là ${LastElement}`)
+
+  }
+getFirstAndLastElement();
+// Tìm tất cả các từ có độ dài từ 4 trở lên trong chuỗi
+// Mô tả: Cho chuỗi "JavaScript is a powerful language", tìm tất cả các từ có độ dài từ 4 trở lên.
+function FindLongWords() {
+  const str ="JavaScript is a powerful language";
+  const LongWords =str.split(' ').filter(words=> words.length >= 4)
+  console.log(` các từ có độ dài từ 4 trở lên ${LongWords}`)
+  }
+  FindLongWords();
+  // Kiểm tra chuỗi có chứa ký tự đặc biệt không
+  // Mô tả: Cho chuỗi "Hello$World!", kiểm tra xem chuỗi này có chứa ký tự đặc biệt như $, !, @ không.
+  function containsSpecialChar() {
+    const str = "Hello$World!";
+     const Special= str.match(/[$!@]/g);
+    console.log("Ký tự đặc biệt:", Special)
+  }
+  containsSpecialChar()
+// Chuyển chuỗi thành dạng Title Case
+// Mô tả: Cho chuỗi "the quick brown fox", chuyển chuỗi này thành dạng "Title Case" là "The Quick Brown Fox".
+function TittleCaseString() {
+  const str="the quick brown fox";
+  const TittleCase= str.split(' ').map(word=>word[0].toUpperCase()+ word.slice(1).toLowerCase()).join(' ')
+  console.log(`Chuyển chuỗi thành dạng Title Case:  ${TittleCase}`)
+  }
+  TittleCaseString()
