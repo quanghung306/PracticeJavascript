84f7374b2d03f49fdb4bd74a0685de57d3eb3c86
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2025-01-08 16:55:15 +0700

    Update: Simplify code using array methods

--- StringAndArrayMethods/ArrayMethods.js
+++ StringAndArrayMethods/ArrayMethods.js
@@ -1,92 +1,79 @@
-var arr = [1, 2, 3, 4, 6, 5, 7];
+const arr = [1, 2, 3, 4, 6, 5, 7];
+const arr1 = [1, 2, 3];
+const arr2 = [2, 3, 4];
+const arr3 = [3, 4, 5];
+const myArr = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
 //Lọc số chẵn trong mảng:
 function EvenNum(arr) {
-  const evenNumbers = arr.filter((arr) => arr % 2 === 0);
-  console.log(`${evenNumbers} là số chẵn trong mảng`);
+  return arr.filter((arr) => arr % 2 === 0);
 }
-EvenNum(arr);
+console.log('số chẵn trong mảng',EvenNum(arr));
+;
 //Nhân đôi giá trị các phần tử trong mảng:
 function DoubleElement(arr) {
-  const doubled = arr.map((arr) => arr * 2);
-  console.log(`Mảng sau khi nhân đôi giá trị là ${doubled} `);
+  return arr.map((arr) => arr * 2);
 }
-DoubleElement(arr);
+console.log('Nhân đôi giá trị các phần tử trong mảng:',DoubleElement(arr));
 //Tính tổng các phần tử trong mảng:
 function SumElement(arr) {
-  const sum = arr.reduce((x, y) => x - y);
-  console.log(`Mảng sau khi tổng các phần tử là ${sum} `);
+  return arr.reduce((sum, num) => sum + num);
 }
-SumElement(arr);
+console.log('Tổng các phần tử trong mảng',SumElement(arr));
+
 //Sắp xếp mảng theo thứ tự giảm dần:
 function SortArr(arr) {
-  const sortNum = [...arr].sort((x, y) => y - x);
-  console.log(`Mảng sau khi sắp xếp theo thứ tự giảm dần là ${sortNum} `);
+  return [...arr].sort((x, y) => y - x);
 }
-SortArr(arr);
+console.log('mảng theo thứ tự giảm dần: ',SortArr(arr));
 //Tìm phần tử đầu tiên lớn hơn 5:
 function Find(arr) {
-  const firstGreaterThanFive = arr.find((x) => x > 5);
-  console.log(`phần tử đầu tiên lớn hơn 5: ${firstGreaterThanFive} `);
+  return arr.find((x) => x > 5);
 }
-Find(arr);
+console.log('phần tử đầu tiên lớn hơn 5:',Find(arr));
+;
 //Cho hai mảng hãy ghép hai mảng thành một
-function MergingArray() {
-  const arr1 = ["John", "Adam"];
-  const arr2 = ["Eva", "Zeus", "Hades"];
-  const MergingArray = arr1.concat(arr2);
-  console.log(`Mảng sau khi ghép là: ${MergingArray}`);
+function MergingArray(str1,str2) {
+  return str1.concat(str2);
 }
-MergingArray();
+const str1 = ["John", "Adam"];
+const str2 = ["Eva", "Zeus", "Hades"];
+console.log('Mảng sau khi ghép là',MergingArray(str1,str2));
+;
 //Tính tổng tất cả các số trong mảng lồng nhau
-function SumArray() {
-  const myArr = [
-    [1, 2],
-    [3, 4],
-    [5, 6],
-  ];
-   const SumArray = myArr.flat().reduce((x, y) => x + y);
-  console.log(`Tông mảng lồng nhau là: ${SumArray}`);
+function SumArray(myArr) {
+  return myArr.flat().reduce((x, y) => x + y);
 }
-SumArray();
+console.log('Tổng tất cả các số trong mảng lồng nhau là',SumArray(myArr));
 //Tìm phần tử chung giữa các mảng
-function FindElement() {
-  const Arr1 =[1, 2, 3];
-  const Arr2 =[2, 3, 4];
-  const Arr3 =[3, 4, 5];
-  const CommonElement = Arr1.filter(element=>Arr2.includes(element) && Arr3.includes(element) )
-  console.log(`Phần tử trùng nhau là : ${CommonElement}`)
-  }
-  FindElement()
+function FindElement(arr1,arr2,arr3) {
+  return arr1.filter((element) => arr2.includes(element) && arr3.includes(element));
+}
+console.log("Phần tử chung giữa các mảng:", FindElement(arr1,arr2,arr3))
 //Tạo mảng mới với mỗi phần tử là tổng các phần tử của các mảng con
-function sumOfSubArrays() {
-  const Arr= [[1, 2], [3, 4], [5, 6]];
-  const SumArry = Arr.map(x=>x.reduce((sum,num) => sum+num))
-  console.log(`tổng các phần tử của các mảng con là: ${SumArry}`)
-  }
-sumOfSubArrays();
+function sumOfSubArrays(myArr) {
+  return myArr.map((arr) => arr.reduce((sum, num) => sum + num));
+}
+console.log('tổng các phần tử của các mảng con: ',sumOfSubArrays(myArr));
+
 //Kiểm tra xem phần tử có tồn tại trong tất cả các mảng
-function CheckElementExist(){
-  const Arr1 =[1, 2, 3];
-  const Arr2 =[2, 3, 4];
-  const Arr3 =[3, 4, 5];
-  const CheckExist = [Arr1, Arr2, Arr3 ].every(array=> array.includes(3))
-  console.log(`phần tử 3 có tồn tại trong tất cả các mảng : ${CheckExist}`)
+function CheckElementExist(arr,element) {
+  return arr.every((array) => array.includes(element));
 }
-CheckElementExist();
+console.log('phần tử có tồn tại trong tất cả các mảng',CheckElementExist([arr1,arr2,arr3],3));
 //Đếm phần tử trong các mảng con
-function CountArrays() {
-  const Arr= [[1, 2], [3, 4], [5, 6]];
-  const SumArry = Arr.map((arr)=>arr.length).reduce((sum,num)=>sum+num);
-  console.log(`Đếm phần tử trong các mảng con là: ${SumArry}`)
+function CountArrays(myArr) {
+  return myArr.map((arr) => arr.length).reduce((sum, num) => sum + num);
 }
-CountArrays();
+console.log(`Phần tử trong các mảng con là ${CountArrays(myArr)}`);
+
 //Lọc phần tử duy nhất từ nhiều mảng
-function FilterElements  () { 
-  const Arr1 =[1, 2, 3];
-  const Arr2 =[3, 4, 5];
-  const Arr3 =[5, 6, 7]
-  const AllElement=[...Arr1,...Arr2,...Arr3]
-  const FilterElements = AllElement.filter(element=>AllElement.lastIndexOf(element) === AllElement.indexOf(element));
-  console.log("phần tử duy nhất từ nhiều mảng:", FilterElements)
- }
- FilterElements  () 
+function FilterElements(arr) {
+  return arr.filter((element) => arr.lastIndexOf(element) === arr.indexOf(element));
+}
+const allElement = [...arr1, ...arr2, ...arr3];
+console.log('phần tử duy nhất từ nhiều mảng',FilterElements(allElement));
+
