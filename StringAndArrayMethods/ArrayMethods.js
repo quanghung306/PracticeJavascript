de6ae7b413a0a0512ae993ce056657dca4ac21cb
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2025-01-09 13:54:16 +0700

    Update: New exercise using array methods

--- StringAndArrayMethods/ArrayMethods.js
+++ StringAndArrayMethods/ArrayMethods.js
@@ -8,72 +8,75 @@ const myArr = [
   [5, 6],
 ];
 //Lọc số chẵn trong mảng:
-function EvenNum(arr) {
+function evenNum(arr) {
   return arr.filter((arr) => arr % 2 === 0);
 }
-console.log('số chẵn trong mảng',EvenNum(arr));
-;
+console.log("số chẵn trong mảng", evenNum(arr));
 //Nhân đôi giá trị các phần tử trong mảng:
-function DoubleElement(arr) {
+function doubleElement(arr) {
   return arr.map((arr) => arr * 2);
 }
-console.log('Nhân đôi giá trị các phần tử trong mảng:',DoubleElement(arr));
+console.log("Nhân đôi giá trị các phần tử trong mảng:", doubleElement(arr));
 //Tính tổng các phần tử trong mảng:
-function SumElement(arr) {
+function sumElement(arr) {
   return arr.reduce((sum, num) => sum + num);
 }
-console.log('Tổng các phần tử trong mảng',SumElement(arr));
+console.log("Tổng các phần tử trong mảng", sumElement(arr));
 
 //Sắp xếp mảng theo thứ tự giảm dần:
-function SortArr(arr) {
+function sortArr(arr) {
   return [...arr].sort((x, y) => y - x);
 }
-console.log('mảng theo thứ tự giảm dần: ',SortArr(arr));
+console.log("mảng theo thứ tự giảm dần: ", sortArr(arr));
 //Tìm phần tử đầu tiên lớn hơn 5:
-function Find(arr) {
+function find(arr) {
   return arr.find((x) => x > 5);
 }
-console.log('phần tử đầu tiên lớn hơn 5:',Find(arr));
-;
+console.log("phần tử đầu tiên lớn hơn 5:", find(arr));
 //Cho hai mảng hãy ghép hai mảng thành một
-function MergingArray(str1,str2) {
+function mergingArray(str1, str2) {
   return str1.concat(str2);
 }
 const str1 = ["John", "Adam"];
 const str2 = ["Eva", "Zeus", "Hades"];
-console.log('Mảng sau khi ghép là',MergingArray(str1,str2));
-;
+console.log("Mảng sau khi ghép là", mergingArray(str1, str2));
 //Tính tổng tất cả các số trong mảng lồng nhau
-function SumArray(myArr) {
+function sumArray(myArr) {
   return myArr.flat().reduce((x, y) => x + y);
 }
-console.log('Tổng tất cả các số trong mảng lồng nhau là',SumArray(myArr));
+console.log("Tổng tất cả các số trong mảng lồng nhau là", sumArray(myArr));
 //Tìm phần tử chung giữa các mảng
-function FindElement(arr1,arr2,arr3) {
-  return arr1.filter((element) => arr2.includes(element) && arr3.includes(element));
+function findElement(arr1, arr2, arr3) {
+  return arr1.filter(
+    (element) => arr2.includes(element) && arr3.includes(element)
+  );
 }
-console.log("Phần tử chung giữa các mảng:", FindElement(arr1,arr2,arr3))
+console.log("Phần tử chung giữa các mảng:", findElement(arr1, arr2, arr3));
 //Tạo mảng mới với mỗi phần tử là tổng các phần tử của các mảng con
 function sumOfSubArrays(myArr) {
   return myArr.map((arr) => arr.reduce((sum, num) => sum + num));
 }
-console.log('tổng các phần tử của các mảng con: ',sumOfSubArrays(myArr));
+console.log("tổng các phần tử của các mảng con: ", sumOfSubArrays(myArr));
 
 //Kiểm tra xem phần tử có tồn tại trong tất cả các mảng
-function CheckElementExist(arr,element) {
+function checkElementExist(arr, element) {
   return arr.every((array) => array.includes(element));
 }
-console.log('phần tử có tồn tại trong tất cả các mảng',CheckElementExist([arr1,arr2,arr3],3));
+console.log(
+  "phần tử có tồn tại trong tất cả các mảng",
+  checkElementExist([arr1, arr2, arr3], 3)
+);
 //Đếm phần tử trong các mảng con
-function CountArrays(myArr) {
+function countArrays(myArr) {
   return myArr.map((arr) => arr.length).reduce((sum, num) => sum + num);
 }
-console.log(`Phần tử trong các mảng con là ${CountArrays(myArr)}`);
+console.log(`Phần tử trong các mảng con là ${countArrays(myArr)}`);
 
 //Lọc phần tử duy nhất từ nhiều mảng
-function FilterElements(arr) {
-  return arr.filter((element) => arr.lastIndexOf(element) === arr.indexOf(element));
+function filterElements(arr) {
+  return arr.filter(
+    (element) => arr.lastIndexOf(element) === arr.indexOf(element)
+  );
 }
 const allElement = [...arr1, ...arr2, ...arr3];
-console.log('phần tử duy nhất từ nhiều mảng',FilterElements(allElement));
-
+console.log("phần tử duy nhất từ nhiều mảng", filterElements(allElement));
