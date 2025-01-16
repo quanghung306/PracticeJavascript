643f9b833dbfb2c4a64abca219a43fab8cf55e91
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2025-01-16 10:23:44 +0700

    Update: fix search todolist

--- ToDoListWeb/toDoList.js
+++ ToDoListWeb/toDoList.js
@@ -14,7 +14,7 @@ function renderTodoList(list) {
         <div class="actions">
         <button class="completeBtn" onclick="completeTodo(${index})">
         ${
-            todo.completed
+          todo.completed
             ? ' <i class="fa-regular fa-circle-xmark"></i>'
             : '<i class="fa-solid fa-check"></i>'
         }
@@ -25,13 +25,26 @@ function renderTodoList(list) {
     todoListElement.appendChild(li);
   });
 }
+function filterTodoList(status) {
+  let filterList = [];
+  if (status === "completed") {
+    filterList = todoList.filter((todo) => todo.completed);
+  } else if (status === "not-completed") {
+    filterList = todoList.filter((todo) => !todo.completed);
+  } else {
+    filterList = todoList;
+  }
+  renderTodoList(filterList);
+  return filterList;
+}
 //add todoList
 function addTodo() {
   const todoName = addInput.value;
+  const currentFilter = filterStatus.value;
   if (todoName) {
     todoList.push({ name: todoName, completed: false });
     addInput.value = "";
-    renderTodoList(todoList);
+    filterTodoList(currentFilter);
   } else {
     alert("Vui lòng nhập tên công việc!");
   }
@@ -46,31 +59,24 @@ function completeTodo(index) {
   todoList[index].completed = !todoList[index].completed;
   renderTodoList(todoList);
 }
-//filter Status todoList
-function filterTodoList(status) {
-  let filerTodo = [];
-  if (status === "completed") {
-    filerTodo = todoList.filter((todo) => todo.completed);
-  } else if (status === "not-completed") {
-    filerTodo = todoList.filter((todo) => !todo.completed);
-  } else {
-    filerTodo = todoList;
-  }
-  renderTodoList(filerTodo);
-}
-filterStatus.addEventListener("change", (event) => {
-  const selectStatus = event.target.value;
-  filterTodoList(selectStatus);
-});
 //sort todoList
 function sortTodoList() {
   todoList.sort((a, b) => a.name.localeCompare(b.name));
   const currentFilter = filterStatus.value;
   filterTodoList(currentFilter);
 }
+//filter Status todoList
+filterStatus.addEventListener("change", (event) => {
+  const selectStatus = event.target.value;
+  filterTodoList(selectStatus);
+});
 //search todoList
 searchInput.addEventListener("input", function (e) {
-  const search = e.target.value;
-  const filteredList = todoList.filter((todo) => todo.name.includes(search));
+  const search = e.target.value.toLowerCase();
+  const currentFilter = filterStatus.value;
+  const filteredByStatus = filterTodoList(currentFilter);
+  const filteredList = filteredByStatus.filter((todo) =>
+    todo.name.toLowerCase().includes(search)
+  );
   renderTodoList(filteredList);
 });
